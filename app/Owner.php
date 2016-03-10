f677cfbfd6c147385f56e2c993221ea40940c046
kristina.gagalovska <kristinagagalovska@gmail.com>
2016-03-10 20:23:55 +0100

    OneToMany

--- app/Owner.php
+++ app/Owner.php
@@ -3,12 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Car;
 
 class Owner extends Model
 {
     public function cars()
     {
-        return $this->hasMany('App\Car');
+        return $this->hasMany('App\Car','owner_id');
     }
 }

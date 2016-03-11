2bd1ca62647adc092b12d5f6243669d4c438db04
kristina.gagalovska <kristinagagalovska@gmail.com>
2016-03-11 06:33:59 +0100

    Owner Firstname instead of Owner Id - cars.index

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -13,7 +13,7 @@
             <td><p>{{ $car->mark }}</p></td>
             <td><p>{{ $car->manufacture }}</p></td>
             <td><p>{{ $car->num }}</p></td>
-            <td><p>{{ json_decode(json_encode(\App\Owner::find($car->owner_id)), True)['firstname'] }}</p></td>
+            <td><p>{{  json_decode(json_encode(\App\Owner::find($car->owner_id)), True)['firstname'] }}</p></td>
             <td><p><a href="{{ url('cars/edit', $car->id) }}">Edit Car</a></p></td>
             <td><p><a href="{{  url('cars/delete', $car->id) }}">Delete Car</a></p></td>
         </tr>

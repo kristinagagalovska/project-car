fdf32a65c5a313604e1703667a22a54b6ac98518
kristina.gagalovska <kristinagagalovska@gmail.com>
2016-03-30 04:40:11 +0200

    The basic views for login, registration and password resets

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -1,11 +1,12 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateRequest;
 use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 use App\Car;
 use App\Owner;
-use Validator;
+
 
 
 class CarController extends Controller
@@ -24,29 +25,22 @@ class CarController extends Controller
         return view('cars.add')->with('owners', $owners);
     }
 
-    public function store(Request $request)
+    public function store(CreateRequest $request)
     {
-        $validator = Validator::make($request->all(), [
-            'model' => 'required|regex:',
-        ]);
-        if ($validator->fails()) {
-            return redirect()->route('cars.add')
-                             ->withErrors($validator);
-        }
         $car = new Car();
         $car->model = $request->get('model');
         $car->mark = $request->get('mark');
         $car->manufacture = $request->get('manufacture');
         $car->num = $request->get('num');
         if ($request->get('owner') == "") {
-            $car->owner_id=null;
+            $car->owner_id = null;
         } else {
-            $car->owner_id=$request->get('owner');
+            $car->owner_id = $request->get('owner');
         }
-        $car->save();
+        Car::create($request->all());
 
         return redirect()->route('cars.index');
-    }
+        }
 
     public function edit($id)
     {
--- resources/views/cars/add.blade.php
+++ resources/views/cars/add.blade.php
@@ -1,4 +1,11 @@
 <h4>Add a new car:</h4>
+@if ($errors->any())
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+@endif
 <form action="/cars/add" method="post">
     <input name="model" placeholder="Enter a model" type="text"/>
     <input name="mark" placeholder="Enter a mark" type="text"/>
